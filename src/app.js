99879cdb69826d286dcd8567adc030bf8f2bcabe
Tyler Howle <t.howle06@outlook.com>
2026-04-29 00:28:44 +0100

    Add basic logger middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { specs, swaggerUi } from "./config/swagger.js";
 import indexRouter from "./routes/index.routes.js";
+import logger from "./middlewares/logger.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/", indexRouter);
